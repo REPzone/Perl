e791aae275cb605b13c61e7369094886784c689b
REPzone <repzone2@gmail.com>
2016-01-12 04:52:44 +0500

    Add task 'mail'

--- Perl4.pl
+++ Perl4.pl
@@ -102,7 +102,7 @@ sub cui{
         print "ID3v1 tag broken or not exist!\n";
         exit 2;
     }
- }
+}
 
 
 sub id3v1_show{
@@ -347,4 +347,103 @@ sub rescan{
     undef @tags;
     id3v1_pars();
     id3v1_show();
+}
+
+sub id3v2_pars{
+    seek($file, 0, 0);
+    read ($file, my $buf, 3);
+    if ($buf eq 'ID3'){
+        %tags;
+        %tagmap = ("AENC" => "Audio encryption",
+              "APIC" => "Attached picture",
+              "COMM" => "Comments",
+              "COMR" => "Commercial frame",
+              "ENCR" => "Encryption method registration",
+              "EQUA" => "Equalization",
+              "ETCO" => "Event timing codes",
+              "GEOB" => "General encapsulated object",
+              "GRID" => "Group identification registration",
+              "IPLS" => "Involved people list",
+              "LINK" => "Linked information",
+              "MCDI" => "Music CD identifier",
+              "MLLT" => "MPEG location lookup table",
+              "OWNE" => "Ownership frame",
+              "PRIV" => "Private frame",
+              "PCNT" => "Play counter",
+              "POPM" => "Popularimeter",
+              "POSS" => "Position synchronisation frame",
+              "RBUF" => "Recommended buffer size",
+              "RVAD" => "Relative volume adjustment",
+              "RVRB" => "Reverb",
+              "SYLT" => "Synchronized lyric/text",
+              "SYTC" => "Synchronized tempo codes",
+              "TALB" => "Album/Movie/Show title",
+              "TBPM" => "BPM (beats per minute)",
+              "TCOM" => "Composer",
+              "TCON" => "Content type",
+              "TCOP" => "Copyright message",
+              "TDAT" => "Date",
+              "TDLY" => "Playlist delay",
+              "TENC" => "Encoded by",
+              "TEXT" => "Lyricist/Text writer",
+              "TFLT" => "File type",
+              "TIME" => "Time",
+              "TIT1" => "Content group description",
+              "TIT2" => "Title/songname/content description",
+              "TIT3" => "Subtitle/Description refinement",
+              "TKEY" => "Initial key",
+              "TLAN" => "Language(s)",
+              "TLEN" => "Length",
+              "TMED" => "Media type",
+              "TOAL" => "Original album/movie/show title",
+              "TOFN" => "Original filename",
+              "TOLY" => "Original lyricist(s)/text writer(s)",
+              "TOPE" => "Original artist(s)/performer(s)",
+              "TORY" => "Original release year",
+              "TOWN" => "File owner/licensee",
+              "TPE1" => "Lead performer(s)/Soloist(s)",
+              "TPE2" => "Band/orchestra/accompaniment",
+              "TPE3" => "Conductor/performer refinement",
+              "TPE4" => "Interpreted, remixed, or otherwise modified by",
+              "TPOS" => "Part of a set",
+              "TPUB" => "Publisher",
+              "TRCK" => "Track number/Position in set",
+              "TRDA" => "Recording dates",
+              "TRSN" => "Internet radio station name",
+              "TRSO" => "Internet radio station owner",
+              "TSIZ" => "Size",
+              "TSRC" => "ISRC (international standard recording code)",
+              "TSSE" => "Software/Hardware and settings used for encoding",
+              "TYER" => "Year",
+              "TXXX" => "User defined text information frame",
+              "UFID" => "Unique file identifier",
+              "USER" => "Terms of use",
+              "USLT" => "Unsychronized lyric/text transcription",
+              "WCOM" => "Commercial information",
+              "WCOP" => "Copyright/Legal information",
+              "WOAF" => "Official audio file webpage",
+              "WOAR" => "Official artist/performer webpage",
+              "WOAS" => "Official audio source webpage",
+              "WORS" => "Official internet radio station homepage",
+              "WPAY" => "Payment",
+              "WPUB" => "Publishers official webpage",
+              "WXXX" => "User defined URL link frame",
+              "TDRC" => "Year",
+              "TCMP" => "Reserve");
+            undef $buf;
+            binmode $data;
+            read ($data,my $buf, 1);
+            #$buf = hex($buf);
+            print pack("h", $buf), "\n";
+            print  "Subversion of ID3v2: $buf \n";
+            seek($file, 6, 0);
+            read($data, $buf, 4);
+            print unpack("H", $buf), "\n";
+            $len = hex($buf);
+            print "Length of ID3v2 tags: $buf \n";
+    }
+    else{
+        print "ID3v2 tag broken or not exist!\n";
+        exit 2;
+    }
 }
